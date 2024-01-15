c4e74a5a10b45424602d6aa3081cc91a1ff71d4e
fdeoses <f.deoses@avoristravel.com>
2024-01-15 17:06:11 +0100

    Standalone Components

--- src/app/alone/components/side-menu/side-menu.component.ts
+++ src/app/alone/components/side-menu/side-menu.component.ts
@@ -1,4 +1,6 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 interface MenuItem {
   name: string;
@@ -6,7 +8,9 @@ interface MenuItem {
 }
 
 @Component({
-  selector: 'maps-side-menu',
+  selector: 'side-menu',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './side-menu.component.html',
   styleUrls: ['./side-menu.component.css'],
 })
@@ -27,6 +31,10 @@ export class SideMenuComponent {
     {
       route: '/maps/properties',
       name: 'Houses',
+    },
+    {
+      route: '/alone',
+      name: 'Alone Page',
     },
   ];
 }
--- src/app/maps/pages/properties-page/properties-page.component.ts
+++ src/app/maps/pages/properties-page/properties-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CounterAloneComponent } from 'src/app/alone/components/counter-alone/counter-alone.component';
 
 interface House {
   title: string;
@@ -7,6 +8,7 @@ interface House {
 }
 
 @Component({
+  // imports: [CounterAloneComponent],
   templateUrl: './properties-page.component.html',
   styleUrls: ['./properties-page.component.css'],
 })
